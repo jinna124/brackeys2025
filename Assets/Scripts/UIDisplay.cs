e4841544da95c59f4a21343be61900072f1dc807
omaroymk <72468295+oymk0@users.noreply.github.com>
2025-09-04 23:46:05 +0300

    Added time elapsed ui

--- Assets/Scripts/UIDisplay.cs
+++ Assets/Scripts/UIDisplay.cs
@@ -12,6 +12,10 @@ public class UIDisplay : MonoBehaviour
     Health playerHealth;
     XPManager XPManager;
 
+
+    // this is for the time text
+    private float elapsed_time = 0f;
+    [SerializeField] TMP_Text timeelapsedtxt;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
@@ -33,5 +37,11 @@ public class UIDisplay : MonoBehaviour
         XPText.text = XPManager.GetXP().ToString() + "/" + XPManager.GetXPRequirement() + " XP";
 
         Debug.Log("Health:" + playerHealth.GetHealth());
+
+        elapsed_time += Time.deltaTime;
+        int minutes = Mathf.FloorToInt(elapsed_time / 60);  
+        int hours = Mathf.FloorToInt(minutes / 60);
+        int seconds = Mathf.FloorToInt(elapsed_time % 60);  // remainder = seconds
+        timeelapsedtxt.text = $"Time Elapsed: {hours:D2}:{minutes:D2}:{seconds:D2}";
     }
 }
