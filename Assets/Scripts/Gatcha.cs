997a8d4f9a80e4c42ce480f5b01d1bf08aa441eb
abdul1ah33 <study.abdullah2005@gmail.com>
2025-09-17 06:04:02 +0300

    added Gatcha.cs

--- Assets/Scripts/Gatcha.cs
+++ Assets/Scripts/Gatcha.cs
@@ -1,16 +1,51 @@
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Gatcha : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private GameObject player;
+    [SerializeField] private Slider gachaslider;
+    private TextMeshProUGUI gachatxt;
+    [SerializeField] GameObject gachatxtgameobject;
+    private TextMeshProUGUI pullcounttxt;
+    [SerializeField] GameObject pullcounttxtgameobject;
+
+    private Health playerHealth;
+
     void Start()
     {
-        
+        if (player != null)
+            playerHealth = player.GetComponent<Health>();
+        if (gachatxtgameobject != null)
+            gachatxt = gachatxtgameobject.GetComponent<TextMeshProUGUI>();
+        if (pullcounttxtgameobject != null)
+        {
+            pullcounttxt = pullcounttxtgameobject.GetComponent<TextMeshProUGUI>();
+        }
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (playerHealth != null)
+        {
+            MaxSliderValue();
+            CurrertText();
+        }
+    }
+    void MaxSliderValue()
+    {
+        if (playerHealth != null)
+            gachaslider.maxValue = playerHealth.GetHealth();
+    }
+    void CurrertText()
+    {
+        gachatxt.text = multiply(((gachaslider.value) / 20), 20).ToString() + " / " + gachaslider.maxValue.ToString("0") + " HP cost";
+        pullcounttxt.text = Mathf.Floor(((gachaslider.value) / 20)).ToString("0") + " pulls";
+    }
+    int multiply(float a, int b)
+    {
+        int c = (int)a;
+        return c * b;
     }
 }
