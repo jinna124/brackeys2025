40307e25c3e3b1ceb07fcb686429b8e6a73f45d4
omaroymk <72468295+oymk0@users.noreply.github.com>
2025-09-17 16:43:22 +0300

    Added the pull HP Cost serialized field to gacha.cs script

--- Assets/Scripts/Gatcha.cs
+++ Assets/Scripts/Gatcha.cs
@@ -9,24 +9,20 @@ public class Gatcha : MonoBehaviour
 {
     [SerializeField] private GameObject player;
     [SerializeField] private Slider gachaSlider;
-    [SerializeField] GameObject gachaTxtGameobject;
-    [SerializeField] GameObject pullCountTxtGameobject;
+    [SerializeField] GameObject HpTextGameobject;
+    [SerializeField] GameObject pullCountTextGameobject;
+    [SerializeField] int pullHpCost = 20;
 
     private Health playerHealth;
     private TextMeshProUGUI gachaTxt;
     private TextMeshProUGUI pullCountTxt;
-    private float previousSliderValue;
 
     void Start()
     {
-        if (player != null)
-            playerHealth = player.GetComponent<Health>();
-        if (gachaTxtGameobject != null)
-            gachaTxt = gachaTxtGameobject.GetComponent<TextMeshProUGUI>();
-        if (pullCountTxtGameobject != null)
-            pullCountTxt = pullCountTxtGameobject.GetComponent<TextMeshProUGUI>();
+        if (player != null) playerHealth = player.GetComponent<Health>();
+        if (HpTextGameobject != null) gachaTxt = HpTextGameobject.GetComponent<TextMeshProUGUI>();
+        if (pullCountTextGameobject != null) pullCountTxt = pullCountTextGameobject.GetComponent<TextMeshProUGUI>();
 
-        previousSliderValue = gachaSlider.value;
         gachaSlider.onValueChanged.AddListener(SliderJump);
     }
     void Update()
@@ -34,6 +30,8 @@ public class Gatcha : MonoBehaviour
         MaxSliderValue();
         CurrertText();
     }
+
+
     void MaxSliderValue()
     {
         if (playerHealth != null)
@@ -42,31 +40,15 @@ public class Gatcha : MonoBehaviour
     void CurrertText()
     {
         gachaTxt.text =
-            ((int)(gachaSlider.value / 20) * 20).ToString()
+            ((int)(gachaSlider.value / pullHpCost) * pullHpCost).ToString()
             + " / "
             + gachaSlider.maxValue.ToString("0")
             + " HP cost";
 
-        pullCountTxt.text = Mathf.Floor(gachaSlider.value / 20).ToString("0") + " pulls";
+        pullCountTxt.text = Mathf.Floor(gachaSlider.value / pullHpCost).ToString("0") + " pulls";
     }
-
     void SliderJump(float currentValue)      
     {
-        float stepSize = 20;
-
-        //if (currentValue + previousSliderValue >= 2)
-        //{
-        //    gachaSlider.value = previousSliderValue + stepSize;
-        //}
-        //else if(Mathf.Abs(currentValue - previousSliderValue) >= 2f) 
-        //{
-        //    gachaSlider.value = previousSliderValue - stepSize;
-        //}
-
-        //gachaSlider.value = Mathf.Clamp(gachaSlider.value, gachaSlider.minValue, gachaSlider.maxValue);
-
-        //previousSliderValue = gachaSlider.value;
-
-        gachaSlider.value = Mathf.Round(currentValue / stepSize) * stepSize;
+        gachaSlider.value = Mathf.Round(currentValue / pullHpCost) * pullHpCost;
     }
 }
