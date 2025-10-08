eeb26a941118f09cceaa7a8b7cb623ecb907ab1f
omaroymk <72468295+oymk0@users.noreply.github.com>
2025-10-08 10:09:29 +0300

    Added some placeholder methods in the objectPooling.cs will optimize later

--- Assets/Scripts/ObjectPooling.cs
+++ Assets/Scripts/ObjectPooling.cs
@@ -1,6 +1,34 @@
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class ObjectPooling : MonoBehaviour
 {
+    [SerializeField] GameObject poolItem;
+    [SerializeField] Queue<GameObject> pool = new Queue<GameObject>();
+    [SerializeField] int poolSize = 20;
     
+    private GameObject go;
+    private void Start()
+    {
+        for (int i = 0; i < poolSize; i++)
+        {
+            go = Instantiate(poolItem);
+            go.SetActive(false);
+            pool.Enqueue(go);
+        }
+    }
+
+    public GameObject GetObject()
+    {
+        go = pool.Dequeue();
+        go.SetActive(enabled);
+        return go;
+    }
+
+    public void ReturnObject(GameObject gameObject)
+    {
+        pool.Enqueue(gameObject);
+        gameObject.SetActive(false);
+    }
 }
